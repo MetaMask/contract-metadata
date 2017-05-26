98e6b01c5ebb4f840b14cc60f6f5e2b8cf784a40
Dan Finlay <dan@danfinlay.com>
2017-05-25 23:11:16 -0700

    First commit: ENS registrar icon

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./icon-map.json')
---
+++ test/index.js
@@ -0,0 +1,29 @@
+const test = require('tape')
+const iconMap = require('../')
+
+const util = require('ethereumjs-util')
+const fs = require('fs')
+const path = require('path')
+
+test('the object is parsable', function (t) {
+  t.equal(typeof iconMap, 'object', 'is an object')
+  t.end()
+})
+
+test('the accounts are valid', function (t) {
+  Object.keys(iconMap).forEach(address => {
+    t.ok(util.isValidAddress(address), `Address should be valid: ${address}`)
+  })
+
+  t.end()
+})
+
+test('value should correspond to a web image file', function (t) {
+  Object.keys(iconMap).forEach(address => {
+    const fileName = iconMap[address]
+    t.ok(fs.existsSync(path.join(__dirname, '..', 'images', fileName)), `file exists: ${fileName}`)
+  })
+
+  t.end()
+})
+
