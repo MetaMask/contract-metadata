881c90e0f86934dafba6a8d4c2c1e98b1b1cdb7f
Michael Hahn <hahn.michael.f@gmail.com>
2025-02-06 12:16:59 -0800

    chore: copy images to new caip-19 folders

---
+++ reformat.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const contractMap = require("./contract-map.json");
+
+const iconsFolder = "./icons";
+const metadataFolder = "./metadata";
+const main = async () => {
+  await Promise.all(
+    Object.entries(contractMap).map(async ([contractAddress, metadata]) => {
+      const caip19ID = `eip155:1/erc20:${contractAddress}`;
+      const iconPath = `${iconsFolder}/${caip19ID}.svg`;
+      const metadataPath = `${metadataFolder}/${caip19ID}.json`;
+      const oldImagesPath = `./images/${metadata.logo}`;
+      if (!fs.existsSync(oldImagesPath)) {
+        console.log(`Image not found: ${oldImagesPath}`);
+
+        return;
+      }
+      console.log(`Folder not found: ${iconsFolder}`);
+
+      if (!fs.existsSync(iconPath)) {
+        console.log(`Folder not found: ${iconsFolder}`);
+        const folder = await fs.promises.mkdir(
+          iconPath.split("/").slice(0, -1).join("/"),
+          { recursive: true },
+        );
+        if (folder) {
+          console.log(`Folder created: ${folder}`);
+        }
+      }
+
+      await fs.promises.copyFile(oldImagesPath, iconPath);
+      if (!fs.existsSync(metadataPath)) {
+        const newMetadataFolder = await fs.promises.mkdir(
+          metadataPath.split("/").slice(0, -1).join("/"),
+          {
+            recursive: true,
+          },
+        );
+        if (newMetadataFolder) {
+          console.log(`Folder created: ${newMetadataFolder}`);
+        }
+      }
+
+      await fs.promises.writeFile(
+        metadataPath,
+        JSON.stringify(
+          {
+            name: metadata.name,
+            symbol: metadata.symbol,
+            decimals: metadata.decimals,
+            erc20: metadata.erc20,
+            logo: `.${iconPath}`,
+          },
+          null,
+          2,
+        ),
+      );
+    }),
+  );
+};
+
+main().then((d) => {
+  console.log("done");
+});
