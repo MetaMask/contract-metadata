5b50f5836ebe01b7c3390d663d05cd74c8c74239
Sam Walker <samuel.walker9@gmail.com>
2025-03-12 20:49:42 -0400

    cleanup

--- generate-multichain-uniswap-standard-list.js
+++ generate-multichain-uniswap-standard-list.js
@@ -2,12 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const contractMap = require('./buildindex.js');
 
-// Get current timestamp in ISO format
 const timestamp = new Date().toISOString();
 
-// Create the Uniswap token list format
 function createUniswapTokenList() {
-  // Transform the metadata into the tokens array format for Uniswap standard
   const tokens = Object.entries(contractMap).map(([key, data]) => {
     // Parse the CAIP-19 ID to extract chain and address information
     // Expected format: eip155:1/erc20:0xAddress
@@ -15,9 +12,6 @@ function createUniswapTokenList() {
     const chainId = parseInt(chainPart.split(':')[1], 10);
     const address = addressPart.split(':')[1];
 
-    // Generate GitHub raw content URL for the logo
-    // The logo path in data is like ./icons/eip155:1/erc20:0xAddress.svg
-    // We want to convert it to a GitHub raw URL
     let logoURI = '';
     if (data.logo) {
       const logoFileName = data.logo.split('/').pop();
@@ -40,8 +34,6 @@ function createUniswapTokenList() {
     logoURI: "https://images.ctfassets.net/clixtyxoaeas/1ezuBGezqfIeifWdVtwU4c/d970d4cdf13b163efddddd5709164d2e/MetaMask-icon-Fox.svg",
     keywords: [
       "metamask",
-      "uniswap",
-      "tokens"
     ],
     timestamp,
     tokens,
@@ -55,10 +47,7 @@ function createUniswapTokenList() {
   return tokenList;
 }
 
-// Generate the Uniswap standard token list
 const tokenList = createUniswapTokenList();
-
-// Write the token list to a file
 const outputPath = path.join(__dirname, 'metamask-uniswap-tokenlist.json');
 
 try {
